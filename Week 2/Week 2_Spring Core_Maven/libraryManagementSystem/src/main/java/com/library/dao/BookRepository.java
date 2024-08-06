6eab633a7ec45bb3aa0e5fd252208e3cadcb009d
Indradev12 <121940034+Indradev12@users.noreply.github.com>
2024-08-06 21:21:48 +0530

    Delete Week 2/Week 2_Spring Core_Maven directory

--- Week 2/Week 2_Spring Core_Maven/libraryManagementSystem/src/main/java/com/library/dao/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.dao;
-
-public class BookRepository {
-	public void update() {
-		System.out.println("Updating Book....");
-	}
-}
--- Week 2/Week 2_Spring Core_Maven/lms(Ex 9)/demo/src/main/java/com/library/demo/dao/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.library.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.library.demo.entities.Book;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-}
